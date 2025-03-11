467062eea78a3fa2267b5f3e45c2723078386a12
Enrique Rivera <enriqueriveranevarez@gmail.com>
2025-03-11 10:17:35 -0600

    Added main file

---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+# https://www.facturacioncfdigm.modelo.gmodelo.com.mx/ModeloFacturaPRD/Modulos/ClienteInternet/ConsultaCFDHistorico.aspx?rfcRec=RINS6910023U7&rfcEmi=AMH080702RMA&idEmi=9&externo=0
+
+# https://www.facturacioncfdigm.modelo.gmodelo.com.mx/ModeloFacturaPRD/Modulos/ClienteInternet/VistaCFDConsulta.aspx?rfcRec=RINS6910023U7&rfcEmi=AMH080702RMA&idEmi=9&externo=0&itu=4663039767cd 
